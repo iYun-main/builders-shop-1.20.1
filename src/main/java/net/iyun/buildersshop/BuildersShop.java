b8ba66a70aafd52bdec813bf1726c5e238e24ca7
iYun-main <modstds@gmail.com>
2024-09-13 18:37:13 +0200

    The beginning

---
+++ src/main/java/net/iyun/buildersshop/BuildersShop.java
@@ -0,0 +1,15 @@
+package net.iyun.buildersshop;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class BuildersShop implements ModInitializer {
+	public static final String MOD_ID = "builders-shop";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+	}
+}
---
+++ src/main/java/net/iyun/buildersshop/ModClient.java
@@ -0,0 +1,10 @@
+package net.iyun.buildersshop;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
