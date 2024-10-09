2195776a9be6ac08e3d44714caf41b68c9675760
iYun-main <thezigix299@gmail.com>
2024-10-09 20:28:19 +0200

    Loottable Done, Block Tags Part 1

--- src/main/java/net/iyun/buildersshop/datagen/ModBlockTagProvider.java
+++ src/main/java/net/iyun/buildersshop/datagen/ModBlockTagProvider.java
@@ -3,6 +3,7 @@ package net.iyun.buildersshop.datagen;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricTagProvider;
 import net.iyun.buildersshop.block.ModBlocks;
+import net.iyun.buildersshop.util.ModTags;
 import net.minecraft.registry.RegistryWrapper;
 import net.minecraft.registry.tag.BlockTags;
 
@@ -33,7 +34,55 @@ public class ModBlockTagProvider extends FabricTagProvider.BlockTagProvider {
                 .add(ModBlocks.BLUE_WOOL_WALL)
                 .add(ModBlocks.PURPLE_WOOL_WALL)
                 .add(ModBlocks.MAGENTA_WOOL_WALL)
-                .add(ModBlocks.PINK_WOOL_WALL);
+                .add(ModBlocks.PINK_WOOL_WALL)
+                .add(ModBlocks.WHITE_WOOL_VERTICAL_SLAB)
+                .add(ModBlocks.LIGHT_GRAY_WOOL_VERTICAL_SLAB)
+                .add(ModBlocks.GRAY_WOOL_VERTICAL_SLAB)
+                .add(ModBlocks.BLACK_WOOL_VERTICAL_SLAB)
+                .add(ModBlocks.BROWN_WOOL_VERTICAL_SLAB)
+                .add(ModBlocks.RED_WOOL_VERTICAL_SLAB)
+                .add(ModBlocks.ORANGE_WOOL_VERTICAL_SLAB)
+                .add(ModBlocks.YELLOW_WOOL_VERTICAL_SLAB)
+                .add(ModBlocks.LIME_WOOL_VERTICAL_SLAB)
+                .add(ModBlocks.GREEN_WOOL_VERTICAL_SLAB)
+                .add(ModBlocks.CYAN_WOOL_VERTICAL_SLAB)
+                .add(ModBlocks.LIGHT_BLUE_WOOL_VERTICAL_SLAB)
+                .add(ModBlocks.BLUE_WOOL_VERTICAL_SLAB)
+                .add(ModBlocks.PURPLE_WOOL_VERTICAL_SLAB)
+                .add(ModBlocks.MAGENTA_WOOL_VERTICAL_SLAB)
+                .add(ModBlocks.PINK_WOOL_VERTICAL_SLAB)
+                .add(ModBlocks.WHITE_WOOL_SLAB)
+                .add(ModBlocks.LIGHT_GRAY_WOOL_SLAB)
+                .add(ModBlocks.GRAY_WOOL_SLAB)
+                .add(ModBlocks.BLACK_WOOL_SLAB)
+                .add(ModBlocks.BROWN_WOOL_SLAB)
+                .add(ModBlocks.RED_WOOL_SLAB)
+                .add(ModBlocks.ORANGE_WOOL_SLAB)
+                .add(ModBlocks.YELLOW_WOOL_SLAB)
+                .add(ModBlocks.LIME_WOOL_SLAB)
+                .add(ModBlocks.GREEN_WOOL_SLAB)
+                .add(ModBlocks.CYAN_WOOL_SLAB)
+                .add(ModBlocks.LIGHT_BLUE_WOOL_SLAB)
+                .add(ModBlocks.BLUE_WOOL_SLAB)
+                .add(ModBlocks.PURPLE_WOOL_SLAB)
+                .add(ModBlocks.MAGENTA_WOOL_SLAB)
+                .add(ModBlocks.PINK_WOOL_SLAB)
+                .add(ModBlocks.WHITE_WOOL_FENCE)
+                .add(ModBlocks.LIGHT_GRAY_WOOL_FENCE)
+                .add(ModBlocks.GRAY_WOOL_FENCE)
+                .add(ModBlocks.BLACK_WOOL_FENCE)
+                .add(ModBlocks.BROWN_WOOL_FENCE)
+                .add(ModBlocks.RED_WOOL_FENCE)
+                .add(ModBlocks.ORANGE_WOOL_FENCE)
+                .add(ModBlocks.YELLOW_WOOL_FENCE)
+                .add(ModBlocks.LIME_WOOL_FENCE)
+                .add(ModBlocks.GREEN_WOOL_FENCE)
+                .add(ModBlocks.CYAN_WOOL_FENCE)
+                .add(ModBlocks.LIGHT_BLUE_WOOL_FENCE)
+                .add(ModBlocks.BLUE_WOOL_FENCE)
+                .add(ModBlocks.PURPLE_WOOL_FENCE)
+                .add(ModBlocks.MAGENTA_WOOL_FENCE)
+                .add(ModBlocks.PINK_WOOL_FENCE);
 
 
         getOrCreateTagBuilder(BlockTags.WALLS)
@@ -63,14 +112,67 @@ public class ModBlockTagProvider extends FabricTagProvider.BlockTagProvider {
                 .add(ModBlocks.CDIRT_WALL);
 
         getOrCreateTagBuilder(BlockTags.FENCES)
+                .add(ModBlocks.WHITE_WOOL_FENCE)
+                .add(ModBlocks.LIGHT_GRAY_WOOL_FENCE)
+                .add(ModBlocks.GRAY_WOOL_FENCE)
+                .add(ModBlocks.BLACK_WOOL_FENCE)
+                .add(ModBlocks.BROWN_WOOL_FENCE)
+                .add(ModBlocks.RED_WOOL_FENCE)
+                .add(ModBlocks.ORANGE_WOOL_FENCE)
+                .add(ModBlocks.YELLOW_WOOL_FENCE)
+                .add(ModBlocks.LIME_WOOL_FENCE)
+                .add(ModBlocks.GREEN_WOOL_FENCE)
+                .add(ModBlocks.CYAN_WOOL_FENCE)
+                .add(ModBlocks.LIGHT_BLUE_WOOL_FENCE)
+                .add(ModBlocks.BLUE_WOOL_FENCE)
+                .add(ModBlocks.PURPLE_WOOL_FENCE)
+                .add(ModBlocks.MAGENTA_WOOL_FENCE)
+                .add(ModBlocks.PINK_WOOL_FENCE)
                 .add(ModBlocks.DEEPSLATE_TILES_FENCE)
                 .add(ModBlocks.CRACKED_DEEPSLATE_TILES_FENCE);
 
 
-        getOrCreateTagBuilder(BlockTags.SHOVEL_MINEABLE)
-                .add(ModBlocks.DIRT_VERTICAL_SLAB)
+        getOrCreateTagBuilder(BlockTags.SLABS)
+                .add(ModBlocks.RDIRT_SLAB)
+                .add(ModBlocks.CDIRT_SLAB)
                 .add(ModBlocks.DIRT_SLAB);
 
+
+        getOrCreateTagBuilder(BlockTags.SHOVEL_MINEABLE)
+                .add(ModBlocks.DIRT_VERTICAL_SLAB)
+                .add(ModBlocks.RDIRT_VERTICAL_SLAB)
+                .add(ModBlocks.CDIRT_VERTICAL_SLAB)
+                .add(ModBlocks.RDIRT_SLAB)
+                .add(ModBlocks.CDIRT_SLAB)
+                .add(ModBlocks.DIRT_SLAB);
+
+        getOrCreateTagBuilder(BlockTags.HOE_MINEABLE);
+
+
+        getOrCreateTagBuilder(BlockTags.PICKAXE_MINEABLE);
+
+
+
+        getOrCreateTagBuilder(ModTags.Blocks.V_SLAB)
+                .add(ModBlocks.WHITE_WOOL_VERTICAL_SLAB)
+                .add(ModBlocks.LIGHT_GRAY_WOOL_VERTICAL_SLAB)
+                .add(ModBlocks.GRAY_WOOL_VERTICAL_SLAB)
+                .add(ModBlocks.BLACK_WOOL_VERTICAL_SLAB)
+                .add(ModBlocks.BROWN_WOOL_VERTICAL_SLAB)
+                .add(ModBlocks.RED_WOOL_VERTICAL_SLAB)
+                .add(ModBlocks.ORANGE_WOOL_VERTICAL_SLAB)
+                .add(ModBlocks.YELLOW_WOOL_VERTICAL_SLAB)
+                .add(ModBlocks.LIME_WOOL_VERTICAL_SLAB)
+                .add(ModBlocks.GREEN_WOOL_VERTICAL_SLAB)
+                .add(ModBlocks.CYAN_WOOL_VERTICAL_SLAB)
+                .add(ModBlocks.LIGHT_BLUE_WOOL_VERTICAL_SLAB)
+                .add(ModBlocks.BLUE_WOOL_VERTICAL_SLAB)
+                .add(ModBlocks.PURPLE_WOOL_VERTICAL_SLAB)
+                .add(ModBlocks.MAGENTA_WOOL_VERTICAL_SLAB)
+                .add(ModBlocks.PINK_WOOL_VERTICAL_SLAB)
+                .add(ModBlocks.RDIRT_VERTICAL_SLAB)
+                .add(ModBlocks.CDIRT_VERTICAL_SLAB)
+                .add(ModBlocks.DIRT_VERTICAL_SLAB);
 
     }
 }
---
+++ src/main/java/net/iyun/buildersshop/util/ModTags.java
@@ -0,0 +1,28 @@
+package net.iyun.buildersshop.util;
+
+import net.iyun.buildersshop.BuildersShop;
+import net.minecraft.block.Block;
+import net.minecraft.item.Item;
+import net.minecraft.registry.RegistryKeys;
+import net.minecraft.registry.tag.TagKey;
+import net.minecraft.util.Identifier;
+
+public class ModTags {
+
+    public static class Blocks {
+
+        public static final TagKey<Block> V_SLAB = createTag("vertical_slabs");
+
+
+        private static TagKey<Block> createTag(String name) {
+            return TagKey.of(RegistryKeys.BLOCK, new Identifier(BuildersShop.MOD_ID, name));
+    }
+}
+    public static class Items {
+
+
+        private static TagKey<Item> createTag(String name) {
+            return TagKey.of(RegistryKeys.ITEM, new Identifier(BuildersShop.MOD_ID, name));
+       }
+    }
+}
